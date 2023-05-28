368ef9f6035a8c654182231ae85949c4c964a016
ikinoiky <01n101292017@gmail.com>
2023-05-28 15:43:32 +0700

    first code

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
 import sys 
-print(sys.argv[0])
+import os
+
+cwd = os.getcwd()
+try:
+    sys.argv[1]
+except:
+    exec(open('./module/noArgs.py'))
+    sys.exit()
+
+args = sys.argv[1]
+if (args == 'build'):
+    try :
+        sys.argv[2]
+    except :
+        print('masukan nama file yang akan di eksekusi')
+        sys.exit()
+    print('checking file...')
+    path = cwd + '/' + sys.argv[2]
+    with open(path, 'r') as f:
+        first_line = f.readline().strip('\n')
+        if (first_line == 'utama' or first_line == 'main'):
+            os.system("python3 " + './module/exec/build.py ' + path)
+            sys.exit()
+        else :
+            print('file =>> "' + sys.argv[2] + '" <<= tidak teridentifikasi sebagai file gabut yang utama')
+            print('baris pertamanya adalah =>> ' + first_line)
+if (args == 'listen'):
+    try :
+        sys.argv[2]
+    except :
+        print('masukan direktori yang akan di pantau')
+        sys.exit()
+    print('Memantau direktori...')
+    path = cwd + '/' + sys.argv[2]
+    
---
+++ module/exec/build.py
@@ -0,0 +1,4 @@
+print('ini didalam build')
+
+import sys 
+print(sys.argv[1])
